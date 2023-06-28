80c933b8783c2b94745d0b1620138818cacc2b5d
Last-SilverLight-End <halfwing1998@gmail.com>
2023-06-28 17:25:24 +0900

    feat : message deny view

--- Components/message_item.tsx
+++ Components/message_item.tsx
@@ -1,8 +1,9 @@
-import { Avatar, Box, Button, Divider, Flex, Text, Textarea } from '@chakra-ui/react';
+import { Avatar, Box, Button, Divider, Flex, IconButton, Menu, MenuButton, MenuItem, MenuList, Spacer, Text, Textarea } from '@chakra-ui/react';
 import ResizeTextArea from 'react-textarea-autosize';
 import { useState } from 'react';
 import { InMessage } from '@/models/message/in_message';
 import convertDateToString from '@/utils/convert_date_toString';
+import MoreBtnIcon from './more_btn_icon';
 
 interface Props {
   uid: string;
@@ -36,7 +37,7 @@ const MessageItem = function ({ uid, displayName, owner, photoURL, item, onSendC
   return (
     <Box borderRadius="md" width="full" bg="white" boxShadow="md">
       <Box>
-        <Flex pl="2" pt="2" alignItems="center">
+        <Flex px="2" pt="2" alignItems="center">
           <Avatar
             size="xs"
             src={item.author ? item.author.photoURL ?? 'https://bit.ly/broken-link' : 'https://bit.ly/broken-link'}
@@ -47,6 +48,21 @@ const MessageItem = function ({ uid, displayName, owner, photoURL, item, onSendC
           <Text whiteSpace="pre-line" fontSize="xx-small" color="gray.300" m="1">
             {convertDateToString(item.createAt)}
           </Text>
+          <Spacer>
+
+          </Spacer>
+          {owner && (
+            <Menu>
+              <MenuButton as={IconButton} icon={<MoreBtnIcon />}
+                width="24px" height="24px" borderRadius="full" variant="link" size="xs" />
+
+              <MenuList>
+                <MenuItem>비공개 처리</MenuItem>
+              </MenuList>
+            </Menu>
+          )
+
+          }
         </Flex>
       </Box>
 
