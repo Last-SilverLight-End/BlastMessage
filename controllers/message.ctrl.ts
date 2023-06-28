1343c1899968ff38c896114d6f03b51b256afb93
Last-SilverLight-End <halfwing1998@gmail.com>
2023-06-28 18:10:18 +0900

    feat : add 거절 api

--- controllers/message.ctrl.ts
+++ controllers/message.ctrl.ts
@@ -1,6 +1,8 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 import MessageModel from '@/models/message/message_model';
 import BadReqError from './error/bad_request_error';
+import CustomServerError from './error/custom_server_error';
+import FirebaseAdmin from '@/models/firebase_admin';
 
 async function post(req: NextApiRequest, res: NextApiResponse) {
   const { uid, message, author } = req.body;
@@ -66,6 +68,38 @@ async function postReply(req: NextApiRequest, res: NextApiResponse) {
   return res.status(201).end();
 }
 
-const MessageCrtl = { post, get, list, postReply };
+async function updateMessage(req: NextApiRequest, res: NextApiResponse) {
+  const token = req.headers.authorization;
+  if (token === undefined) {
+    throw new CustomServerError({ statusCode: 401, message: '권한이 없습니다' });
+  }
+  let tokenUid: null | string = null;
+  try {
+    const decode = await FirebaseAdmin.getInstance().Auth.verifyIdToken(token);
+    tokenUid = decode.uid;
+  }
+  catch (err) {
+    throw new BadReqError('token에 문제가 발생했습니다.');
+  }
+  const { uid, messageId, deny } = req.body;
+
+
+  if (uid === undefined) {
+    throw new BadReqError('uid가 존재하지 않습니다.');
+  }
+  if (uid !== tokenUid) {
+    throw new CustomServerError({ statusCode: 401, message: '수정할 권한이 없습니다' });
+  }
+  if (messageId === undefined) {
+    throw new BadReqError('메세지 아이디가 존재하지 않습니다.');
+  }
+  if (deny === undefined) {
+    throw new BadReqError('reply가 누락되어 있습니다.');
+  }
+  const result = await MessageModel.updateMessage({ uid, messageId, deny });
+  return res.status(200).json(result);
+}
+
+const MessageCrtl = { post, get, updateMessage, list, postReply };
 
 export default MessageCrtl;
---
+++ pages/api/message_deny.ts
@@ -0,0 +1,18 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+
+import { NextApiRequest, NextApiResponse } from 'next';
+import handleError from '@/controllers/error/handle_error';
+import checkSupportMethod from '@/controllers/error/check_support_method';
+import MessageCrtl from '@/controllers/message.ctrl';
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  const { method } = req;
+  const supportedMethod = ['PUT'];
+  try {
+    checkSupportMethod(supportedMethod, method);
+    await MessageCrtl.updateMessage(req, res);
+  } catch (err) {
+    console.error(err);
+    handleError(err, res);
+  }
+}
